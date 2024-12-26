b418c09b1ae9e84f6b0b7f9634b0118392f162bf
vukvuk21 <vukvuk21@gmail.com>
2024-12-26 23:15:59 +0100

    Added context, and roles entity, ConfigurePersistence dependecy injection

---
+++ SciConnect/IdentityServer/IdentityService/Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using IdentityServer.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Data
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ SciConnect/IdentityServer/IdentityService/Entities/Roles.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityServer.Entities;
+
+public static class Roles 
+{
+    public const string Buyer = "Buyer";
+    public const string Admin = "Administrator";
+}
