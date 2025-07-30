6073d50a39a9074b24cc6fbb6f3f5e7978cf834a
Nikola Stojanovic <nikola.st715@gmail.com>
2025-07-30 12:19:07 +0200

    Dodata email notifikaciona funkcionalnost za slanje i generisanje admin registracionog verifikacionog koda

---
+++ SciConnect/IdentityServer/IdentityService/Services/EmailSender.cs
@@ -0,0 +1,46 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using MailKit.Security;
+
+namespace IdentityService.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _config;
+
+        public EmailSender(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendAsync(string to, string subject, string body)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(
+                _config["EmailSettings:DisplayName"],
+                _config["EmailSettings:From"]
+            ));
+            message.To.Add(MailboxAddress.Parse(to));
+            message.Subject = subject;
+            message.Body = new TextPart("html") { Text = body };
+
+            using var smtp = new SmtpClient();
+
+            var server = _config["EmailSettings:SmtpServer"];
+            var port = int.Parse(_config["EmailSettings:Port"]);
+            var from = _config["EmailSettings:From"];
+            var password = _config["EmailSettings:Password"];
+            var useSSL = bool.Parse(_config["EmailSettings:UseSSL"]);
+            var useStartTls = bool.Parse(_config["EmailSettings:UseStartTls"]);
+
+            SecureSocketOptions options = useStartTls ? SecureSocketOptions.StartTls :
+                                            useSSL ? SecureSocketOptions.SslOnConnect :
+                                            SecureSocketOptions.None;
+
+            await smtp.ConnectAsync(server, port, options);
+            await smtp.AuthenticateAsync(from, password);
+            await smtp.SendAsync(message);
+            await smtp.DisconnectAsync(true);
+        }
+    }
+}
---
+++ SciConnect/IdentityServer/IdentityService/Services/IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace IdentityService.Services
+{
+    public interface IEmailSender
+    {
+        Task SendAsync(string to, string subject, string body);
+    }
+}
