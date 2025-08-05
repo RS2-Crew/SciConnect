3a220b0c0fadeff0d9c6d12895dbc4045190bda9
Sloba98 <slobacar635@gmail.com>
2025-08-05 21:56:43 +0200

    Style changes

--- SciConnect/WebApps/SciConnectSPA/src/app/identity/identity-routing.module.ts
+++ SciConnect/WebApps/SciConnectSPA/src/app/identity/identity-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IdentityComponent } from './identity.component';
 import { LoginFormComponent } from './feature-authentification/login-form/login-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
-  { path: '', component: IdentityComponent, children: [{path: 'login', component: LoginFormComponent}] }
+  {
+    path: '',
+    component: IdentityComponent,
+    children: [
+      { path: 'login', component: LoginFormComponent }
+    ]
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes), ReactiveFormsModule],
-  exports: [RouterModule],
-  bootstrap: [LoginFormComponent]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class IdentityRoutingModule { }
